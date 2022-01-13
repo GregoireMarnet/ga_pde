d97c2687026877e677e1cfe6c9f72b3ac658b75d
gregmrnt <gregmarnet@hotmail.fr>
2022-01-13 18:26:57 +0100

    update

--- rate.cpp
+++ rate.cpp
@@ -6,9 +6,27 @@ namespace dauphine
 
 rate::rate(){};
 
-double rate::operator() (int i)
+rate_BS::rate_BS (double r)
+{   
+    p_rate.resize(1);
+    p_rate[0] = r;
+}
+
+
+rate_gen::rate_gen (double r, dauphine::mesh msh, double alpha)
 {
-    return rate_vector[i]
+    const int n_dt = msh.get_ndt();
+    p_rate.resize(n_dt);
+    p_rate[0] = r;
+    for(int i=1; i<n_dt ; i++){
+            p_rate[i] = p_rate[i-1] + alpha;
+        };
+}
+
+
+std::vector<double> rate::get_rates() const
+{
+    return p_rate;
 }
 
 
